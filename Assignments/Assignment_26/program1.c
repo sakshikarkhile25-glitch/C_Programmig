c00d49bb50995cea7e4f9dc067957fdb2f5b3d7a
sakshikarkhile25-glitch <sakshikarkhile25@gmail.com>
2025-11-21 14:50:06 +0530

    Assignment_26 Solution (Q1 to Q5)

---
+++ Assignments/Assignment_26/program1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    char ch = 'A';
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%c ", ch);
+        ch++;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
+    
