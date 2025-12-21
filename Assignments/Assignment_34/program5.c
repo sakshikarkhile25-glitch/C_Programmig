13e3cff52365fe434cbe51e0bb3ee16878e2225b
sakshikarkhile25-glitch <sakshikarkhile25@gmail.com>
2025-12-21 11:45:45 +0530

    Add files via upload

---
+++ Assignments/Assignment_34/program5.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void DisplayASCII(char ch)
+{
+    int iCnt = 0;
+
+    for(int iCnt = 0;iCnt <= 255; iCnt++)
+    {
+        if(ch == iCnt)
+        {
+            printf("%c  %d  %x  %o\n",iCnt,iCnt,iCnt,iCnt);
+        } 
+    }
+    
+}
+int main()
+{
+    char cValue = '\0';
+
+    printf("Enter the Character:");
+    scanf("%c",&cValue);
+
+    DisplayASCII(cValue);
+
+    return 0;
+}
